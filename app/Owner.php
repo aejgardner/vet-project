418730406526089d301e1a7c551f156798d8b46d
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-12 17:11:08 +0100

    Header, footer and welcome message created

--- app/Owner.php
+++ app/Owner.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    public function fullName()
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    public function fullAddress()
+    {
+        return $this->address_1 . " " . $this->address_2;
+    }
+
+    public function formattedPhoneNumber()
+    {
+        return substr($this->telephone, 0, -7) . " " . substr($this->telephone, 4, -4) . " " . substr($this->telephone, -4);
+    }
 }

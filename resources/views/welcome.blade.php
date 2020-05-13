09d46cfc99222e23d0a8613f7d076cb8d57d0a66
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-13 10:27:51 +0100

    Header and footer broken up into partials, owners names listed on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Veterinary Clinic</title>
-</head>
-<body>
-    
-    <header>
-        <h3>Veterinary Clinic</h3>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+@extends('app')
 
-    <div class="container">
-        <main>
-            <h1>Welcome to the Veterinary Clinic</h1>
-        </main>
-    </div>
+@section('title', 'Veterinary Clinic')
 
+@section('content')
+    <h1>Welcome to the Veterinary Clinic</h1>
 
-    <footer>
-        <p>&copy Veterinary Clinic 2020</p>
-    </footer>
-
-</body>
-</html>
+    @foreach (App\Owner::all() AS $owner)
+        <p>
+            {{ $owner->fullName() }}
+        </p>
+    @endforeach
+@endsection
